e3c3c03cc2962bf55deac5eef6f9ccf2931b8bdf
Stas <staskolodyuk@gmail.com>
2016-10-02 18:55:22 +0300

    Make API Methods Async

--- src/main/java/by/kolodyuk/controller/ResourceController.java
+++ src/main/java/by/kolodyuk/controller/ResourceController.java
@@ -6,16 +6,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @RestController
 @RequestMapping(path = "/resources")
 public class ResourceController
 {
     @GetMapping(path = "/{resourceId}")
-    public Resource getResource(@PathVariable String resourceId)
+    public Callable<Resource> getResource(@PathVariable String resourceId)
     {
         Resource resource = new Resource();
         resource.setId(resourceId);
 
-        return resource;
+        return () -> resource;
     }
 }
