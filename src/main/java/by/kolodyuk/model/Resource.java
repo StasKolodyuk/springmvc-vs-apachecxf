22fb7f9800d0936df0ff7f7b416fbafa5fdc7875
Stas <staskolodyuk@gmail.com>
2016-10-02 18:52:08 +0300

    Create Model

---
+++ src/main/java/by/kolodyuk/model/Resource.java
@@ -0,0 +1,17 @@
+package by.kolodyuk.model;
+
+
+public class Resource
+{
+    public String id;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+}
---
+++ src/main/java/by/kolodyuk/model/Subresource.java
@@ -0,0 +1,28 @@
+package by.kolodyuk.model;
+
+
+public class Subresource
+{
+    private String id;
+    private String resourceId;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getResourceId()
+    {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId)
+    {
+        this.resourceId = resourceId;
+    }
+}
