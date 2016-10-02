2265ef2689f35972a63e7e679a5eb37c01eed528
Stas <staskolodyuk@gmail.com>
2016-10-02 18:51:58 +0300

    Create Spring Boot App Stub

---
+++ src/main/java/by/kolodyuk/SpringMvcVsApacheCxf.java
@@ -0,0 +1,13 @@
+package by.kolodyuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcVsApacheCxf
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringMvcVsApacheCxf.class, args);
+    }
+}
