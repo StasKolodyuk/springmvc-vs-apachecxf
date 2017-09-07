1c15f94ee6e5c859c393a5a7fa591384d0851881
Stas Kolodyuk <staskolodyuk@gmail.com>
2017-09-08 00:05:27 +0300

    create lol method

--- src/main/java/by/alittlebitmoredemocracynotonlykolodyukbutsenkovichaswell/controller/subresource/SubresourceController.java
+++ src/main/java/by/alittlebitmoredemocracynotonlykolodyukbutsenkovichaswell/controller/subresource/SubresourceController.java
@@ -22,4 +22,8 @@ public class SubresourceController {
         subresource.setResourceId(resourceId);
         return subresource;
     }
+
+    public void lol() {
+        // sdofsdfopdsfpdfsp
+    }
 }
